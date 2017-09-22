0b644b11e9c305a33e505525f4b9cdde341ea4ea
cwy007 <chanweiyan007@gmail.com>
2017-09-22 23:30:38 +0800

    administrative user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,7 +8,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should redirect index when not logged in" do
     get users_path
-    assert_redirected_to login_url 
+    assert_redirected_to login_url
   end
 
   test "should get new" do
@@ -27,5 +27,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                               email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: { user: { password:              "password",
+                                                    password_confirmation: "password",
+                                                    admin: true } }
+    assert_not @other_user.reload.admin?                                                
   end
 end
