b935aabec18a11f99825b95b04d285007b7013bd
cwy007 <chanweiyan007@gmail.com>
2017-09-22 17:02:21 +0800

    add edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user 
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
