8414fd95bbdce15ccf3ceba6402119dadf32674e
cwy007 <chanweiyan007@gmail.com>
2017-09-24 00:19:26 +0800

    Activation test and refactoring

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,18 +5,23 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin = users(:michael)
     @non_admin = users(:archer)
+    @unactivated = users(:chanweiyan)
   end
 
-  test "index as admin including pagination and delete links" do
+  test "index as admin including pagination and delete links and activated users" do
     log_in_as(@admin)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), text: user.name, count: 0
       end
     end
     assert_difference 'User.count', -1 do
@@ -24,9 +29,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "index as non-admin" do
+  test "index as non-admin and only display avtivated users" do
     log_in_as(@non_admin)
     get users_path
-    assert_select 'a', text: 'delete', count: 0 
+    assert_select 'a', text: 'delete', count: 0
+    get user_path(@unactivated)
+    assert_redirected_to root_url
   end
 end
