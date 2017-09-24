5f57ad31e35686124fbd5e0f04803406695d4161
cwy007 <chanweiyan007@gmail.com>
2017-09-25 00:10:12 +0800

    Password reset test

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,7 +7,6 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
-    @user = User.find_by(email: params[:email])
   end
 
   def create
@@ -29,6 +28,7 @@ class PasswordResetsController < ApplicationController
       render 'edit'
     elsif @user.update_attributes(user_params)
       log_in @user
+      @user.update_attribute(:reset_digest, nil)
       flash[:success] = "Password has been reset."
       redirect_to @user
     else
