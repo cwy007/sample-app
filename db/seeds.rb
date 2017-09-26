c1d5fe4f23c049a426a7021226576e9574963799
cwy007 <chanweiyan007@gmail.com>
2017-09-27 01:19:39 +0800

    User/relationship associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.delete_all
+User.delete_all if User.any?
 
 User.create!(name: "Example User",
              email: "example@railstutorial.org",
