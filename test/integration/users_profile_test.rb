0c7b505411913fe488b4cc68999b911bcd8521a9
cwy007 <chanweiyan007@gmail.com>
2017-09-27 17:32:13 +0800

    Stats and a follow form

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -7,7 +7,9 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
   end
 
   test "profile display" do
-    get user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to user_path(@user)
+    follow_redirect!
     assert_template 'users/show'
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
@@ -17,5 +19,10 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+    get root_path
+    assert_select 'a[href=?]', following_user_path(@user)
+    assert_select 'a[href=?]', followers_user_path(@user)
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
   end
 end
